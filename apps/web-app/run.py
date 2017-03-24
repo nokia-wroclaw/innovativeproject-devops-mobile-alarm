a77063089c38fe20f26172e53c213a43acb53ad9
Marcin Okroy <218176@student.pwr.edu.pl>
2017-03-24 15:21:59 +0100

    Clean up
    Adding comments

--- apps/web-app/run.py
+++ apps/web-app/run.py
@@ -1,13 +1,25 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, login_required
 import views
 
 app = Flask(__name__, template_folder='templates')
+
+#config for Forms
 app.config.update(DEBUG = True, SECRET_KEY = 'csefdfdf')
+
+#import aplication views
 app.register_blueprint(views.devops)
+
+#conection to database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
+#flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 def run():
     app.run(debug=True)
