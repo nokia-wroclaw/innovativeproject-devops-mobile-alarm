bca9c80769d1b453306d52a8fcb97b3c0a8ab151
Jakub <jakbat@onet.pl>
2017-03-30 15:35:26 +0200

    added ServiceGenerator; clean up

---
+++ apps/android-app/app/src/main/java/pwr/android_app/model/ServiceGenerator.java
@@ -0,0 +1,30 @@
+package pwr.android_app.model;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    private static final String BASE_URL = "https://devops-nokia.herokuapp.com";
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    private static HttpLoggingInterceptor logging =
+            new HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    public static <S> S createService(Class<S> serviceClass){
+
+        return retrofit.create(serviceClass);
+    }
+
+}
--- apps/android-app/app/src/main/java/pwr/android_app/view/TestingFragment.java
+++ apps/android-app/app/src/main/java/pwr/android_app/view/TestingFragment.java
@@ -21,6 +21,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import okhttp3.OkHttpClient;
 import pwr.android_app.R;
+import pwr.android_app.model.ServiceGenerator;
 import pwr.android_app.model.dataStructures.UserData;
 import pwr.android_app.model.interfaces.DevOpsClient;
 import retrofit2.Call;
@@ -31,85 +32,49 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TestingFragment extends Fragment {
 
-    // --- DATA --- //
-    private Button test1Button = null;
-    private TextView test1TextView = null;
+    /* ========================================== DATA ========================================== */
+    // Used in REST requests
+    private DevOpsClient client = null;
 
+    // UI references
     private Button testJsonButton = null;
     private TextView testJsonView = null;
 
-
-    // --- METHODS --- //
+    /* ========================================= METHODS ======================================== */
+    // --- CONSTRUCTOR --- //
     public TestingFragment() {
         // Required empty public constructor
     }
 
-
-
+    // --- ON CREATE VIEW --- //
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // [Retrofit]
+        this.client = ServiceGenerator.createService(DevOpsClient.class);
+
 
         View view = inflater.inflate(R.layout.fragment_testing, container, false);
 
-        test1Button = (Button) view.findViewById(R.id.test1_button);
-        test1TextView = (TextView) view.findViewById(R.id.test1_textview);
-
+        // UI references
         testJsonButton = (Button) view.findViewById(R.id.test_json_button);
         testJsonView = (TextView) view.findViewById(R.id.test_json_view);
 
 
-        test1Button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                try {
-                    // =============================================================================
-                    RequestService rs = new RequestService(getString(R.string.API_BASE_URL) + "/test");
-
-                    rs.execute((Void) null);
-                    // ----
-                    test1TextView.setText(rs.getOutputData());
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                // =================================================================================
-            }
-        });
-
+        // Test request
         testJsonButton.setOnClickListener((new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-                Retrofit.Builder builder =
-                        new Retrofit.Builder()
-                                .baseUrl(getString(R.string.API_BASE_URL))
-                                .addConverterFactory(
-                                        GsonConverterFactory.create()
-                                );
-
-                Retrofit retrofit =
-                        builder
-                                .client(
-                                        httpClient.build()
-                                )
-                                .build();
-
-                DevOpsClient client =  retrofit.create(DevOpsClient.class);
-
-                // Fetch a list of the Github repositories.
                 Call<UserData> call = client.doTestJson();
 
-// Execute the call asynchronously. Get a positive or negative callback.
                 call.enqueue(new Callback<UserData>() {
                     @Override
                     public void onResponse(Call<UserData> call, Response<UserData> response) {
                         testJsonView.setText(response.body().getUserEmail());
-                        String cookie = response.headers().get("Set-cookie");
                     }
 
                     @Override
@@ -120,78 +85,7 @@ public class TestingFragment extends Fragment {
             }
         }));
 
-
         // Inflate the layout for this fragment
         return view;
-    }
-
-
-
-    // Testowy request do serwera (tez co zwraca kapibarę)
-    private class RequestService extends AsyncTask<Void, Void, Boolean> {
-
-        // --- DATA --- //
-        private URL url;                        // Adres zapytania.
-        private String outputData;              // Dane otrzymane po zapytaniu.
-
-        // --- CONSTRUCTOR --- //
-        RequestService(String url) throws MalformedURLException {
-            this.url = new URL(url);            // Ustawia URL na który zostanie wysłane żadanie.
-        }
-
-        // --- GETTERS --- //
-        public String getOutputData() {
-            return this.outputData;
-        }
-
-        // --- METHODS --- //
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            try {
-                // === Ustanawianie połączenia === //
-                HttpsURLConnection connection;
-                connection = (HttpsURLConnection) this.url.openConnection();
-                connection.connect();
-
-                // === Pobranie odpowiedzi z serwera === //
-                if (connection.getResponseCode() == 200) {
-                    InputStream stream = connection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                    StringBuffer buffer = new StringBuffer();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        buffer.append(line);
-                    }
-                    this.outputData = buffer.toString();
-                }
-                else {
-                    this.outputData = "ERROR";
-                }
-
-                // === Zamykanie połączenia === //
-                if (connection != null) {
-                    connection.disconnect();
-                }
-
-                // === TODO Obsługa wyjątów === //
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return false;
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-                return false;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-
-            test1TextView.setText(this.getOutputData());
-        }
     }
 }
