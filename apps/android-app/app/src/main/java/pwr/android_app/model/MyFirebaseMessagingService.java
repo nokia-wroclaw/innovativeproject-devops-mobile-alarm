1541a90b32868a44cab7cae003cb7737a207c086
Jakub <jakbat@onet.pl>
2017-04-08 18:52:29 +0200

    added notifications framework

---
+++ apps/android-app/app/src/main/java/pwr/android_app/model/MyFirebaseMessagingService.java
@@ -0,0 +1,40 @@
+package pwr.android_app.model;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import pwr.android_app.R;
+import pwr.android_app.view.MainActivity;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    // Metoda wywoływana za każdym razem podczas odebrania notyfikacji z serwera FCM
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Intent i = new Intent(this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this,0,i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.logo)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(0,builder.build());
+    }
+}
