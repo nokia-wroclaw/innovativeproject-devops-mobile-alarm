1d3b5857b520bcce1443e9ce712c6be29d824767
Jakub <jakbat@onet.pl>
2017-04-09 19:13:15 +0200

    updated notifications

--- apps/android-app/app/src/main/java/pwr/android_app/network/notifications/MyFirebaseMessagingService.java
+++ apps/android-app/app/src/main/java/pwr/android_app/network/notifications/MyFirebaseMessagingService.java
@@ -1,8 +1,10 @@
 package pwr.android_app.network.notifications;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,7 +16,6 @@ import pwr.android_app.view.activities.MainActivity;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    // Metoda wywoływana za każdym razem podczas odebrania notyfikacji z serwera FCM
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
@@ -26,12 +27,16 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         NotificationCompat.Builder builder =
                 (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setAutoCancel(true)
-                .setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody())
-                .setSmallIcon(R.mipmap.icon)
-                .setContentIntent(pendingIntent);
-
+                .setAutoCancel(true)                                                                // Make this notification automatically dismissed when the user touches it.
+                .setShowWhen(true)                                                                  // Show the timestamp
+                .setVibrate(new long[] { 1000, 1000 })                                              // Set vibrations
+                .setLights(Color.YELLOW, 500, 3000)                                                 // Turn on LED
+                .setContentTitle(remoteMessage.getNotification().getTitle())                        // Set the first line of text (TITLE) in the platform notification template.
+                .setContentText(remoteMessage.getNotification().getBody())                          // Set the second line of text (MESSAGE TEXT) in the platform notification template.
+                .setSmallIcon(R.drawable.icon)                                                      // Set the small icon resource, which will be used to represent the notification in the status bar.
+                .setColor(getColor(R.color.dark_grey))                                              // Set the background, where the small icon is
+                .setContentIntent(pendingIntent);                                                   // Supply a PendingIntent to be sent when the notification is clicked.
+                                                                                                    // [source: https://developer.android.com/reference/android/app/Notification.Builder.html]
         NotificationManager manager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
