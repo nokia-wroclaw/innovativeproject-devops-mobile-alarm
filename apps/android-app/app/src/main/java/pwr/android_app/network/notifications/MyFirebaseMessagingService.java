757ac1c1b2b2c6a501b6c051bbfe32fe8fdea48e
Jakub <jakbat@onet.pl>
2017-04-13 17:10:19 +0200

    modified notifications

--- apps/android-app/app/src/main/java/pwr/android_app/network/notifications/MyFirebaseMessagingService.java
+++ apps/android-app/app/src/main/java/pwr/android_app/network/notifications/MyFirebaseMessagingService.java
@@ -5,12 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import pwr.android_app.R;
+import pwr.android_app.view.activities.LoginActivity;
 import pwr.android_app.view.activities.MainActivity;
 
 
@@ -19,24 +21,39 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        Intent i = new Intent(this, MainActivity.class);
+        super.onMessageReceived(remoteMessage);
+        Intent i = new Intent(this, LoginActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
         PendingIntent pendingIntent =
                 PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        // creating String with notification Title
+        int notificationTitleId = getResources().getIdentifier(remoteMessage.getNotification().getTitleLocalizationKey(),"string", getPackageName());
+        String[] notificationTitleArgs = remoteMessage.getNotification().getTitleLocalizationArgs();
+        String notificationTitle = getResources().getString(notificationTitleId, notificationTitleArgs);
+
+        // creating String with notification Body
+        int notificationBodyId = getResources().getIdentifier(remoteMessage.getNotification().getBodyLocalizationKey(), "string", getPackageName());
+        String[] notificationBodyArgs = remoteMessage.getNotification().getBodyLocalizationArgs();
+        String notificationBody = getResources().getString(notificationBodyId, notificationBodyArgs );
+
+        // preparing icon id
+        int notificationIconId = getResources().getIdentifier(remoteMessage.getNotification().getIcon(), "drawable", getPackageName());
+
+
         NotificationCompat.Builder builder =
                 (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-					.setAutoCancel(true)                                                                // Make this notification automatically dismissed when the user touches it.
-					.setShowWhen(true)                                                                  // Show the timestamp
-					.setVibrate(new long[] { 1000, 1000 })                                              // Set vibrations
-					.setLights(Color.YELLOW, 500, 3000)                                                 // Turn on LED
-					.setContentTitle(remoteMessage.getNotification().getTitle())                        // Set the first line of text (TITLE) in the platform notification template.
-					.setContentText(remoteMessage.getNotification().getBody())                          // Set the second line of text (MESSAGE TEXT) in the platform notification template.
-					.setSmallIcon(R.drawable.icon)                                                      // Set the small icon resource, which will be used to represent the notification in the status bar.
-					.setColor(getColor(R.color.dark_grey))                                              // Set the background, where the small icon is
-					.setContentIntent(pendingIntent);                                                   // Supply a PendingIntent to be sent when the notification is clicked.
-																										// [source: https://developer.android.com/reference/android/app/Notification.Builder.html]
+					.setAutoCancel(true)
+					.setShowWhen(true)
+					.setVibrate(new long[] { 1000, 1000, 100 })
+					.setLights(Color.YELLOW, 500, 3000)
+					.setContentTitle(notificationTitle)
+					.setContentText(notificationBody)
+					.setSmallIcon(notificationIconId)
+					.setColor(getColor(R.color.dark_grey))
+					.setContentIntent(pendingIntent);
+
         NotificationManager manager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
