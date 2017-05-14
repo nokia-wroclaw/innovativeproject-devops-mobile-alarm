e0653682859e5f064ed7e8e8990af232ed3ef8c1
GO <218401@student.pwr.edu.pl>
2017-05-14 12:53:50 +0200

    PieChart.
    New chart has been added.

--- apps/android-app/app/src/main/java/pwr/android_app/view/fragments/MainMenuFragment.java
+++ apps/android-app/app/src/main/java/pwr/android_app/view/fragments/MainMenuFragment.java
@@ -1,10 +1,26 @@
 package pwr.android_app.view.fragments;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import pwr.android_app.R;
 
 public class MainMenuFragment extends Fragment {
@@ -29,7 +45,47 @@ public class MainMenuFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
 
+        displayChart(view);
+
         return view;
+    }
+
+    void displayChart(View view) {
+        // TODO: Butterknife ~ make it Jacob
+        PieChart chart = (PieChart) view.findViewById(R.id.chart);
+        List<PieEntry> entries = new ArrayList<>();
+        List<Integer> colors = new ArrayList<>();
+
+        float isUp = 3f;
+        float isDown = 1f;
+        float isUnspecified = 1f;
+
+        String[] labels = { "Up", "Down", "Unspecified" };
+
+        entries.add(new PieEntry(isUp, labels[0]));
+        entries.add(new PieEntry(isDown, labels[1]));
+        entries.add(new PieEntry(isUnspecified, labels[2]));
+
+        colors.add(ContextCompat.getColor(getContext(), R.color.service_up_color));
+        colors.add(ContextCompat.getColor(getContext(), R.color.service_down_color));
+        colors.add(ContextCompat.getColor(getContext(), R.color.service_unspecified_color));
+
+        PieDataSet dataSet = new PieDataSet(entries, "");
+        dataSet.setColors(colors);
+        dataSet.setSliceSpace(10f);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(18f);
+
+        chart.setData(data);
+        chart.animateY(1000);
+        chart.setCenterText("STATES");
+        chart.setHighlightPerTapEnabled(false);
+        chart.setHoleColor(ContextCompat.getColor(getContext(), R.color.light_grey));
+
+        Description desc = new Description();
+        desc.setText("");
+        chart.setDescription(desc);
     }
 
     /* ========================================================================================== */
